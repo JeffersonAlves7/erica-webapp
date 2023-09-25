1abe330f95edc25cdfe2beca8e8d5ec641d68bd3
JeffersonAlves7 <ilj031420@hotmail.com>
2023-09-25 10:53:19 -0300

    feat: create modal to confirm delete product

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,9 @@
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogOverlay,
   Box,
   Button,
   CloseButton,
@@ -13,7 +18,8 @@ import {
   Td,
   Th,
   Thead,
-  Tr
+  Tr,
+  useDisclosure
 } from "@chakra-ui/react";
 import { format } from "date-fns";
 import { useRef, useState } from "react";
@@ -54,10 +60,15 @@ const itemExample: ItemHome = {
 };
 
 export function Home() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [estoque, setEstoque] = useState<Estoque>("Geral");
   const [items, setItems] = useState<ItemHome[]>([itemExample]);
+  const [itemIdToDelete, setItemIdToDelete] = useState<ItemHome["id"] | null>(
+    null
+  );
   const importadoraInput = useRef<HTMLInputElement>(null);
   const codigoInput = useRef<HTMLInputElement>(null);
+  const cancelRef = useRef(null);
 
   function handleChangeEstoque(estoqueFromButton: Estoque) {
     if (estoqueFromButton == estoque) return;
@@ -73,8 +84,9 @@ export function Home() {
     console.log({ importadoraValue, codigoValue });
   }
 
-  function handleDeleteItem(itemId: ItemHome['id']){
-    setItems(items => items.filter(item => item.id !== itemId));
+  function handleDeleteItem() {
+    setItems((items) => items.filter((item) => item.id !== itemIdToDelete));
+    onClose();
   }
 
   const qntDeCaixas = items.reduce<number | ItemHome>((previous, current) => {
@@ -86,115 +98,151 @@ export function Home() {
   }, 0) as number;
 
   return (
-    <Stack gap={10} h={"full"}>
-      <Heading>Estoques</Heading>
+    <>
+      <Stack gap={10} h={"full"}>
+        <Heading>Estoques</Heading>
 
-      {/* Seletor de estoques */}
-      <Stack direction={"row"} gap={4}>
-        {estoques.map((currentEstoque) => (
+        {/* Seletor de estoques */}
+        <Stack direction={"row"} gap={4}>
+          {estoques.map((currentEstoque) => (
+            <Button
+              key={currentEstoque + "-button"}
+              onClick={() => handleChangeEstoque(currentEstoque)}
+              w={150}
+              backgroundColor={
+                estoque == currentEstoque ? "erica.green" : "erica.pink"
+              }
+              _hover={{ opacity: 0.7 }}
+              size={"md"}
+            >
+              {currentEstoque}
+            </Button>
+          ))}
+        </Stack>
+
+        {/* Filtros */}
+        <Stack direction={"row"} gap={4} align={"center"}>
+          <FormControl w={150}>
+            <FormLabel>
+              <p className="text-xs">Filtrar por Importadora</p>
+            </FormLabel>
+            <Input
+              placeholder="Ex.: Y888"
+              ref={importadoraInput}
+              onKeyDown={(e) => {
+                if (e.key.toLowerCase() === "enter") {
+                  handleSearchPedidos();
+                }
+              }}
+            />
+          </FormControl>
+          <FormControl w={150}>
+            <FormLabel>
+              <p className="text-xs">Filtrar por Código</p>
+            </FormLabel>
+            <Input
+              placeholder={"Ex.: BT0001"}
+              ref={codigoInput}
+              onKeyDown={(e) => {
+                if (e.key.toLowerCase() === "enter") {
+                  handleSearchPedidos();
+                }
+              }}
+            />
+          </FormControl>
           <Button
-            key={currentEstoque + "-button"}
-            onClick={() => handleChangeEstoque(currentEstoque)}
-            w={150}
-            backgroundColor={
-              estoque == currentEstoque ? "erica.green" : "erica.pink"
-            }
             _hover={{ opacity: 0.7 }}
-            size={"md"}
+            backgroundColor={"erica.green"}
+            marginTop={6}
+            onClick={handleSearchPedidos}
           >
-            {currentEstoque}
+            <BsSearch />
           </Button>
-        ))}
-      </Stack>
+        </Stack>
 
-      {/* Filtros */}
-      <Stack direction={"row"} gap={4} align={"center"}>
-        <FormControl w={150}>
-          <FormLabel>
-            <p className="text-xs">Filtrar por Importadora</p>
-          </FormLabel>
-          <Input
-            placeholder="Ex.: Y888"
-            ref={importadoraInput}
-            onKeyDown={(e) => {
-              if (e.key.toLowerCase() === "enter") {
-                handleSearchPedidos();
-              }
-            }}
-          />
-        </FormControl>
-        <FormControl w={150}>
-          <FormLabel>
-            <p className="text-xs">Filtrar por Código</p>
-          </FormLabel>
-          <Input
-            placeholder={"Ex.: BT0001"}
-            ref={codigoInput}
-            onKeyDown={(e) => {
-              if (e.key.toLowerCase() === "enter") {
-                handleSearchPedidos();
-              }
-            }}
-          />
-        </FormControl>
-        <Button
-          _hover={{ opacity: 0.7 }}
-          backgroundColor={"erica.green"}
-          marginTop={6}
-          onClick={handleSearchPedidos}
-        >
-          <BsSearch />
-        </Button>
-      </Stack>
-
-      {/* Tabela de Produtos */}
-      <Box overflowX={"scroll"}>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>Código</Th>
-              <Th>Entrada</Th>
-              <Th>Saldo Atual</Th>
-              <Th>Container</Th>
-              <Th>Importadora</Th>
-              <Th>Data de Entrada</Th>
-              <Th>Dias em Estoque</Th>
-              <Th>Apagar</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {items.map((item) => (
-              <Tr key={"item-" + item.sku}>
-                <Td>{item.sku}</Td>
-                <Td>{item.quantidadeEntrada}</Td>
-                <Td>{item.saldo}</Td>
-                <Td>{item.container}</Td>
-                <Td>{item.importadora}</Td>
-                <Td>{format(item.dataDeEntrada, "dd/MM/yyyy")}</Td>
-                <Td>{item.diasEmEstoque} dia(s)</Td>
-                <Td>
-                  <Flex align={"center"} justify={"center"}>
-                    <CloseButton
-                    onClick={() => handleDeleteItem(item.id)}
-                      backgroundColor={"red.400"}
-                      _hover={{
-                        opacity: 0.7
-                      }}
-                    />
-                  </Flex>
-                </Td>
+        {/* Tabela de Produtos */}
+        <Box overflowX={"scroll"}>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Código</Th>
+                <Th>Entrada</Th>
+                <Th>Saldo Atual</Th>
+                <Th>Container</Th>
+                <Th>Importadora</Th>
+                <Th>Data de Entrada</Th>
+                <Th>Dias em Estoque</Th>
+                <Th>Apagar</Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </Box>
+            </Thead>
+            <Tbody>
+              {items.map((item) => (
+                <Tr key={"item-" + item.sku}>
+                  <Td>{item.sku}</Td>
+                  <Td>{item.quantidadeEntrada}</Td>
+                  <Td>{item.saldo}</Td>
+                  <Td>{item.container}</Td>
+                  <Td>{item.importadora}</Td>
+                  <Td>{format(item.dataDeEntrada, "dd/MM/yyyy")}</Td>
+                  <Td>{item.diasEmEstoque} dia(s)</Td>
+                  <Td>
+                    <Flex align={"center"} justify={"center"}>
+                      <CloseButton
+                        onClick={() => {
+                          setItemIdToDelete(item.id);
+                          onOpen();
+                        }}
+                        backgroundColor={"red.400"}
+                        _hover={{
+                          opacity: 0.7
+                        }}
+                      />
+                    </Flex>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </Box>
 
-      {/* Resumo da quantidade */}
-      <Box justifySelf={"flex-end"}>
-        <span>
-          {items.length} Produto(s) | Total de {qntDeCaixas} caixas.
-        </span>
-      </Box>
-    </Stack>
+        {/* Resumo da quantidade */}
+        <Box justifySelf={"flex-end"}>
+          <span>
+            {items.length} Produto(s) | Total de {qntDeCaixas} caixas.
+          </span>
+        </Box>
+
+        {/* Modal para confirmar deletar produto */}
+        <AlertDialog
+          isOpen={isOpen}
+          onClose={onClose}
+          leastDestructiveRef={cancelRef}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogBody>
+                <p className="text-2xl font-semibold">
+                  Você realmente deseja excluir esse item? Atenção! Essa ação
+                  não pode ser desfeita
+                </p>
+              </AlertDialogBody>
+              <AlertDialogFooter>
+                <Button colorScheme="red" ref={cancelRef} onClick={onClose}>
+                  Cancelar
+                </Button>
+                <Button
+                  colorScheme="green"
+                  backgroundColor={"erica.green"}
+                  onClick={handleDeleteItem}
+                  ml={3}
+                >
+                  Confirmar
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </Stack>
+    </>
   );
 }
