cd193a427ff46267aeaa050bb25ae2271694f93b
JeffersonAlves7 <ilj031420@hotmail.com>
2023-12-02 15:04:26 -0300

    feat: scroll in table component

--- frontend/src/components/customTable.tsx
+++ frontend/src/components/customTable.tsx
@@ -2,7 +2,7 @@ import { Box, Table, TableProps } from "@chakra-ui/react";
 
 export function CustomTable(props: TableProps) {
   return (
-    <Box overflow={"auto"}>
+    <Box overflow={"auto"} height={"50vh"} minH={300}>
       <Table {...props}>{props.children}</Table>
     </Box>
   );
